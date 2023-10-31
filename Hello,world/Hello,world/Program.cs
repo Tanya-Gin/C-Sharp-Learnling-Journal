b110f8f3daeb78f0d7d4da3c13389148ed0730ee
Tanya <118675890+Tanya-Gin@users.noreply.github.com>
2023-10-31 19:49:49 +0800

    Add files via upload

---
+++ Hello,world/Hello,world/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello，world");                //这一行是输出hello world
+            /*
+         　  　▃▆█▇▄▖
+  　 　 　 ▟◤▖　　　◥█▎
+     　 ◢◤　 ▐　　　 　▐▉
+  　 ▗◤　　　▂　▗▖　　▕█▎
+  　◤　▗▅▖◥▄　▀◣　　█▊
+  ▐　▕▎◥▖◣◤　　　　◢██
+  █◣　◥▅█▀　　　　▐██◤
+  ▐█▙▂　　     　◢██◤
+  ◥██◣　　　　◢▄◤
+   　　▀██▅▇▀
+
+
+  哲学♂ 114514
+           */
+            Console.ReadLine();                               //这一行保持控制台
+         
+        }
+        
+    }
+}
