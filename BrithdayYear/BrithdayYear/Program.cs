f349c442b40f831a03f20b01f19be705fe4f8545
Tanya <118675890+Tanya-Gin@users.noreply.github.com>
2023-10-31 19:53:57 +0800

    Add files via upload

---
+++ BrithdayYear/BrithdayYear/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrithdayYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入你的出生年份");
+            int brithdayyear = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入你的第n个生日");
+            int n = int.Parse(Console.ReadLine());
+            int c;
+                c = brithdayyear + n;
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
