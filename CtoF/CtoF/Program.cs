81205b97d1e1824aa3e50286f9b4e4f0245e9859
Tanya <118675890+Tanya-Gin@users.noreply.github.com>
2023-10-31 20:00:12 +0800

    Add files via upload

---
+++ CtoF/CtoF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtoF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celsius;
+            Console.Write("请输入温度(℃):");
+            Celsius = double.Parse(Console.ReadLine());
+            double Fahrenheit = (9 / 5) * Celsius + 32;
+            Console.WriteLine($"您输入的℃温度为:{Celsius}");
+            Console.WriteLine($"您输入的℃对应的℉为:{Fahrenheit}");
+            Console.ReadLine();
+        }
+       
+    }
+
+}
