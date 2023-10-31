b1bd35022d3636befecd087aa466db07331dc64f
Tanya <118675890+Tanya-Gin@users.noreply.github.com>
2023-10-31 19:53:13 +0800

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c;
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
