38c9e643b4ccfb8dc023e74ad88620a7065accbc
Tanya <118675890+Tanya-Gin@users.noreply.github.com>
2023-10-31 19:57:40 +0800

    Add files via upload

---
+++ Volume/Volume/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volume
+{
+    internal class Program
+    {
+        public const double PI = 3.1415926;
+        static void Main(string[] args)
+        {
+            double r;
+            Console.Write("请输入半径;");
+            r= double.Parse(Console.ReadLine());
+            double h;
+            Console.Write("请输入高:");
+            h= double.Parse(Console.ReadLine());
+            double SSercal = r * r * PI;
+            Console.WriteLine($"您的底面积为;{SSercal}");
+            double Volume = SSercal * h;
+            Console.WriteLine($"您的体积为;{Volume}");
+            Console.ReadLine();
+
+        }
+    }
+}
