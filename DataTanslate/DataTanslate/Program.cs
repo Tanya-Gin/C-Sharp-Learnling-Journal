139e8ea5ef8939c6427156ca1723edb0713c542a
Tanya <118675890+Tanya-Gin@users.noreply.github.com>
2023-10-31 19:59:11 +0800

    Add files via upload

---
+++ DataTanslate/DataTanslate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTranslate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int MilkPrice = 65;
+            int PortPrice = 16;
+            int TotalPrice;
+            TotalPrice = MilkPrice * 2 + PortPrice * 3;
+            Console.WriteLine(TotalPrice);
+            double Price = TotalPrice * 0.88;
+            Console.WriteLine(Price);
+            Console.ReadLine();
+            
+        }
+        
+    }
+}
