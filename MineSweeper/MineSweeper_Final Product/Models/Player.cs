ef819b8d4a24584e25a3f5b4903764f97713fa3f
Tanya <118675890+Tanya-Gin@users.noreply.github.com>
2024-10-30 12:11:32 +0800

    Add files via upload

---
+++ MineSweeper/MineSweeper_Final Product/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper1.Models
+{
+    public class Player
+    {
+        public int Score { get; set; }
+        public string PlayerName {  get; set; }
+        public string GameDate {  get; set; }
+    }
+}
